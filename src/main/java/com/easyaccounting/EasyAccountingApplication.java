621015c98004fb2ea32c050f83f67dbba6bc5276
ahmetosan <ahmetpcf@gmail.com>
2022-05-23 17:47:30 -0400

    First Commit

---
+++ src/main/java/com/easyaccounting/EasyAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.easyaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAccountingApplication.class, args);
+    }
+
+}
