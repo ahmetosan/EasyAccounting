69c868dd3bc6b0bd66ba519969ec2c64c693029d
musafir928 <uganoghli@gmail.com>
2022-05-26 00:22:41 +0200

    add payment entity

---
+++ src/main/java/com/easyaccounting/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.easyaccounting.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Where(clause = "is_deleted=false")
+public class Payment extends BaseEntity{
+
+    private String month;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate year;
+    private Double amount;
+    private Boolean isPaid;
+    private Long createdBy;
+    private LocalDateTime createdTime;
+    private Long updatedBy;
+    private LocalDateTime updatedTime;
+    private String institutionId;
+
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Company company;
+}
