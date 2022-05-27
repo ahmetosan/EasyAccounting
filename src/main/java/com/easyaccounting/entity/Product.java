da3f372d9209061e598569fd4957d98f7d538cb1
Abrehett <seseney2022@gmail.com>
2022-05-27 01:26:37 -0400

    Product entity

--- src/main/java/com/easyaccounting/entity/Product.java
+++ src/main/java/com/easyaccounting/entity/Product.java
@@ -8,7 +8,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import java.time.LocalDate;
+
 
 @NoArgsConstructor
 @Data
