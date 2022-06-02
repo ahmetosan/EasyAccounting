670fb6e4c74b6a3add1570954b7dd2b356ca5e58
BetulB <betul.bogrek@gmail.com>
2022-06-02 18:49:27 -0500

    some updates

--- src/main/java/com/easyaccounting/service/impl/PurchaseInvoiceServiceImpl.java
+++ src/main/java/com/easyaccounting/service/impl/PurchaseInvoiceServiceImpl.java
@@ -1,6 +1,8 @@
 package com.easyaccounting.service.impl;
 
 import com.easyaccounting.dto.PurchaseInvoiceDTO;
+import com.easyaccounting.mapper.MapperUtil;
+import com.easyaccounting.mapper.PurchaseInvoiceMapper;
 import com.easyaccounting.service.PurchaseInvoiceService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,14 @@ import java.util.List;
 
 @Service
 public class PurchaseInvoiceServiceImpl implements PurchaseInvoiceService {
+
+    private final PurchaseInvoiceMapper purchaseInvoiceMapper;
+    private final MapperUtil mapperUtil;
+
+    public PurchaseInvoiceServiceImpl(PurchaseInvoiceMapper purchaseInvoiceMapper, MapperUtil mapperUtil) {
+        this.purchaseInvoiceMapper = purchaseInvoiceMapper;
+        this.mapperUtil = mapperUtil;
+    }
 
     @Override
     public List<PurchaseInvoiceDTO> listAllPurchaseInvoices() {
