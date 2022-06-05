914af3620417d6e8750e3ef3b02d20becf6a61e1
BetulB <betul.bogrek@gmail.com>
2022-06-05 11:04:52 -0500

    updates on thymeleaf on purchaseInvoiceList

--- src/main/java/com/easyaccounting/controller/PurchaseInvoiceController.java
+++ src/main/java/com/easyaccounting/controller/PurchaseInvoiceController.java
@@ -6,8 +6,11 @@ import com.easyaccounting.service.PurchaseInvoiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/invoice")
 public class PurchaseInvoiceController {
 
     private final PurchaseInvoiceService invoiceService;
@@ -16,16 +19,23 @@ public class PurchaseInvoiceController {
         this.invoiceService = invoiceService;
     }
 
-    @GetMapping("/invoice/purchase-invoice-list.html")
+    @GetMapping("/purchase-invoice-list")
     public String getPurchaseInvoices(Model model){
         model.addAttribute("purchaseInvoices", invoiceService.listAllPurchaseInvoices(InvoiceType.PURCHASE));
         model.addAttribute("purchaseInvoice", new PurchaseInvoiceDTO());
-        return "invoice/purchase-invoice-list";
+        return "/invoice/purchase-invoice-list";
     }
 
-    @GetMapping("/invoice/invoice/purchase-invoice-list/")
-    public String getDashboardPurchaseInvoices(Model model){
-        model.addAttribute("purchaseInvoices", invoiceService.listAllPurchaseInvoices(InvoiceType.PURCHASE));
-        return "invoice/purchase-invoice-list";
+    @GetMapping("/purchase-invoice-create")
+    public String createPurchaseInvoice(Model model){
+        model.addAttribute("invoice", new PurchaseInvoiceDTO());
+        return "/invoice/purchase-invoice-create";
+    }
+
+    // refactor when implementing crud
+    @GetMapping("/approve/{purchaseInvoiceNumber}")
+    public String approvePurchaseInvoice(@PathVariable("purchaseInvoiceNumber") String purchaseInvoiceNumber) {
+        invoiceService.approvePurchaseInvoice(purchaseInvoiceNumber);
+        return "redirect:/invoice/purchase-invoice-list";
     }
 }
