25443fbdb26dd778e64068e9ff0f79cb1ef61488
musafir928 <uganoghli@gmail.com>
2022-06-06 03:02:02 +0200

    add Payment repository

---
+++ src/main/java/com/easyaccounting/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.easyaccounting.repository;
+
+import com.easyaccounting.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findAllByYear(String year);
+}
